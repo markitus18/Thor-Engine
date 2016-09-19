003e29ecde4229c8981e83042f9d10955337c938
markitus18 <marc18.garrigo@gmail.com>
2016-09-19 20:35:06 +0200

    Lil bit of playing :D

--- Source Code/UI_Console.cpp
+++ Source Code/UI_Console.cpp
@@ -27,7 +27,7 @@ void UI_Console::Draw(const char* title, bool* p_open)
 {
 	if (ImGui::Begin("Console", p_open, ImVec2(500, 300), 1.0f, 0))
 	{
-		ImColor col = ImColor(1.0f, 1.0f, 1.0f, 1.0f);
+		ImColor col = ImColor(0.6f, 0.6f, 1.0f, 1.0f);
 		ImGui::PushStyleColor(0, col);
 
 		ImGui::TextUnformatted(buffer.begin());

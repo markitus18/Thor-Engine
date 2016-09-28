572ca28c9a9ca95d25a0e70318df85469b26783a
markitus18 <marc18.garrigo@gmail.com>
2016-09-28 17:02:28 +0200

    Module render copy

---
+++ ModuleRenderer3D.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "glmath.h"
+#include "Light.h"
+
+#define MAX_LIGHTS 8
+
+class ModuleRenderer3D : public Module
+{
+public:
+	ModuleRenderer3D(Application* app, bool start_enabled = true);
+	~ModuleRenderer3D();
+
+	bool Init();
+	update_status PreUpdate(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+
+	void OnResize(int width, int height);
+
+public:
+
+	Light lights[MAX_LIGHTS];
+	SDL_GLContext context;
+	mat3x3 NormalMatrix;
+	mat4x4 ModelMatrix, ViewMatrix, ProjectionMatrix;
+
+private:
+	float* vertices;
+	uint* index;
+
+	uint  vertex_id;
+	uint  index_id;
+
+	float* tri_vertex;
+	uint* tri_index;
+
+	uint tri_vertex_id;
+	uint tri_index_id;
+};

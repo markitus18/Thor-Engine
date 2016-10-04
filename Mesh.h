b7840612cd9c1f6a214737e1a71c0ddcb63854a8
markitus18 <marc18.garrigo@gmail.com>
2016-10-04 23:47:22 +0200

    tmp copy of conflicted files

--- Mesh.h
+++ Mesh.h
@@ -1,25 +1,36 @@
 #ifndef __MESH_H__
 #define __MESH_H__
 
-#include "Glew\"
+#include "Globals.h"
+
 class Mesh
 {
 public:
 	Mesh();
 	~Mesh();
-
+	void LoadData(char* path);
+	void LoadBuffers();
+	void Draw();
 public:
+	//Vertices data
 	uint	id_vertices = 0;
 	uint	num_vertices = 0;
 	uint*	indices = NULL;
 
+	//Indices data
 	uint	id_indices = 0;
 	uint	num_indices = 0;
 	float*	vertices = NULL;
 
+	//Normals data
 	uint	id_normals = 0;
 	uint	num_normals = 0;
 	float*	normals = NULL;
+
+	//Texture coords
+	uint	id_tex_coords = 0;
+	uint	num_tex_coords = 0;
+	float*	tex_coords = NULL;
 };
 
 #endif

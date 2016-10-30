58425badbc3d9175fd3db53a4bf16311dca6c9ec
markitus18 <marc18.garrigo@gmail.com>
2016-10-30 13:14:00 +0100

    Optimized method not working yet
    Problem with line002 and plane001 when getting farthesPoint

--- Source Code/Intersections.cpp
+++ Source Code/Intersections.cpp
@@ -68,7 +68,7 @@ bool Intersects(const Frustum& frustum, const AABB& box, uint timer)
 
 bool Intersects(const Plane* planes, const AABB& box, uint timer)
 {
-	bool optimized = false;
+	bool optimized = true;
 
 	if (!optimized)
 	{
@@ -98,9 +98,7 @@ bool Intersects(const Plane* planes, const AABB& box, uint timer)
 		{
 			float3 farthestPoint = float3::zero;
 			float3 normal = planes[p].normal;
-			farthestPoint.x = normal.x >= 0 ? box.maxPoint.x : box.minPoint.x;
-			farthestPoint.y = normal.y >= 0 ? box.maxPoint.y : box.minPoint.y;
-			farthestPoint.z = planes[p].d >= 0 ? box.maxPoint.z : box.minPoint.z;
+			farthestPoint.x = normal.x > 0 ? box.maxPoint.x : box.minPoint.x;
 
 			if (planes[p].normal.Dot(farthestPoint) - planes[p].d >= 0.f)
 			{

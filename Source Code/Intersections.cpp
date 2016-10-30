48aa3bbb7da4817447c1ebcf715b1669bcab1a4b
markitus18 <marc18.garrigo@gmail.com>
2016-10-30 19:01:32 +0100

    Camera culling optimized working

--- Source Code/Intersections.cpp
+++ Source Code/Intersections.cpp
@@ -98,9 +98,9 @@ bool Intersects(const Plane* planes, const AABB& box, uint timer)
 		{
 			float3 farthestPoint = float3::zero;
 			float3 normal = planes[p].normal;
-			farthestPoint.x = normal.x > 0 ? box.maxPoint.x : box.minPoint.x;
-			farthestPoint.y = normal.y > 0 ? box.maxPoint.y : box.minPoint.y;
-			farthestPoint.z = planes[p].d > 0 ? box.maxPoint.z : box.minPoint.z;
+			farthestPoint.x = -normal.x >= 0 ? box.maxPoint.x : box.minPoint.x;
+			farthestPoint.y = -normal.y >= 0 ? box.maxPoint.y : box.minPoint.y;
+			farthestPoint.z = -normal.z >= 0 ? box.maxPoint.z : box.minPoint.z;
 
 			if (planes[p].normal.Dot(farthestPoint) - planes[p].d >= 0.f)
 			{

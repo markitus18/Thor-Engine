d48c670906b95959709b774c2ebe0f6d58e17f8c
markitus18 <marc18.garrigo@gmail.com>
2016-11-03 01:21:22 +0100

    Added parson

---
+++ Source Code/Config.h
@@ -0,0 +1,16 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include "parson\parson.h"
+
+class Config
+{
+	/*
+	Save a JSON file into a buffer then create a new file with fileSystem:
+		JSON_Status json_serialize_to_buffer(const JSON_Value *value, char *buf, size_t buf_size_in_bytes);
+	
+	
+	*/
+};
+
+#endif //__CONFIG_H__
